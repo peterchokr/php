519609b5325b632207d25901e6e6b6d8f698b494
peterchokr <61734237+peterchokr@users.noreply.github.com>
2020-06-29 17:02:30 +0900

    .

---
+++ 소스/07/view3.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<meta charset="utf-8">
+<link href="style.css" rel="stylesheet">
+</head>
+<body>
+<?php
+	$content = $_POST["content"];
+	
+	// $content = str_replace("\n","<br>",$_POST["content"]);
+?>
+	<ul>
+		<li>글 내용 : <?= $content?></li>
+	</ul>
+</body>
+</html>
